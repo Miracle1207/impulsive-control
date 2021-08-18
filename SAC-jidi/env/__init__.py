bf51b1e2417dcf87b3e53366f4b1c5e225242b2e
Miracle1207 <70636870+Miracle1207@users.noreply.github.com>
2021-08-18 19:24:56 +0800

    Add files via upload

---
+++ SAC-jidi/env/__init__.py
@@ -0,0 +1,12 @@
+# from .snakes import *
+# from .reversi import *
+# from .gobang import *
+# from .sokoban import *
+from .ccgame import *
+# from .football import *
+# from .MiniWorld import *
+# from .minigrid import *
+# from .particleenv import *
+# from .overcookedai import *
+# from .magent import *
+# from .gridworld import *
